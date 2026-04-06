ec556b46bdce64f955230a330f56f7022a8a8de5
John Schnase <jschnase@mac.com>
2026-04-06 10:06:33 -0600

    Updated package structure

--- R/utils-globals.R
+++ R/utils-globals.R
@@ -1,7 +1,10 @@
 utils::globalVariables(c(
-  "x", 
-  "y", 
-  "trend", 
-  ".data", 
+  "x",
+  "y",
+  "trend",
+  ".data",
   "value"
 ))
+
+#' @importFrom rENM.core rENM_project_dir get_species_info show_species show_variables
+NULL
